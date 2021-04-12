0858ce84f53b0451ba309f30cabc48f447e27653
Fábio Reis <espynho23@gmail.com>
2021-04-12 16:31:55 -0300

    concluido

--- exercicio_aula6/script.js
+++ exercicio_aula6/script.js
@@ -11,10 +11,39 @@ console.log(res);
 // criando um servidor
 http.createServer(function(requisicao, resposta){
     resposta.writeHead(200,{'Content-Type':'text/html'});
-    resposta.write("O loko meu!!!");
-    //let q  = url.parse(requisicao.url, true).query;
+    let retorno  = url.parse(requisicao.url, true).query;
     //let soma = Number(q.n1+q.n2);
     //let soma = parseInt(q.n1) + parseInt(q.n2);
+    let entrada1 = parseInt(retorno.numero1); // variavel recebendo numero vindo do formulario
+    let entrada2 = parseInt(retorno.numero2); // variavel recebendo numero vindo do formulario
+    let operacao = retorno.operacao; // variavel recebendo valor do formulario
+    
+    let resultado ;
+    switch(operacao){
+        case 'somar':
+            resultado = operacoes.soma(entrada1,entrada2);
+            resposta.write("Resultado da soma: " + resultado);
+            break;
+        case 'sub':
+            resultado = operacoes.subtracao(entrada1,entrada2);
+            resposta.write("Resultado da subtração: " + resultado);
+            break;
+        case 'mul':
+            resultado = operacoes.multiplicacao(entrada1,entrada2);
+            resposta.write("Resultado da multiplicação: " + resultado);
+            break;
+        case 'div':
+            resultado = operacoes.divisao(entrada1,entrada2);
+            resposta.write("Resultado da divisão: " + resultado);
+            break;
+    }
+    
+   // let soma = operacoes.soma(entrada1,entrada2);
+
+    console.log(resultado);
+    
+    
+
     //let txt = "Numeros recebidos " + soma;
     //resposta.write(requisicao.url);
     //resposta.end(txt);
