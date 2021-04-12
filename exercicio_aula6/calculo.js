8b6d6e42543c56e9d71afbf39a28b7944231bcb5
Fábio Reis <espynho23@gmail.com>
2021-04-12 11:36:52 -0300

    link operações com codigo principal

--- exercicio_aula6/calculo.js
+++ exercicio_aula6/calculo.js
@@ -19,3 +19,6 @@ let operacoes = {
     }
 }
 
+module.exports = operacoes;
+
+
---
+++ exercicio_aula6/script.js
@@ -0,0 +1,7 @@
+"use strict"
+const operacoes = require('./calculo'); // importação do codigo das operações matematicas
+
+
+let res = operacoes.soma(5,6);
+
+console.log(res);
