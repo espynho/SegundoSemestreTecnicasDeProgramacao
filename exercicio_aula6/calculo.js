fef9f36aff584a0f379e380aa199f7156a03d5bd
Fábio Reis <espynho23@gmail.com>
2021-04-12 11:07:58 -0300

    operações matematicas

---
+++ exercicio_aula6/calculo.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// operações matematicas
+let operacoes = {
+    soma : function(a,b){
+        return a+b;
+    },
+
+    subtracao : function(a,b){
+        return a-b;
+    },
+
+    multiplicacao : function(a,b){
+        return a*b;
+    },
+
+    divisao : function(a,b){
+        return a/b;
+    }
+}
+
